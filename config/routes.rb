1c8829f0ff51d6d31ca3cbf53c8e98519100e57f
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-03-06 17:38:22 +0100

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 HvO::Application.routes.draw do
 
+  devise_for :users
+	devise_scope :user do
+		get "login", :to => "devise/sessions#new"
+		get "logout", :to => "devise/sessions#destroy"
+	end
+
   resources :questions do
 	  resources :items
 	  resources :comments
