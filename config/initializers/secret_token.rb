4136cb1b23ab2098b643d885f753dd9a821592cc
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-03-06 11:39:06 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HvO::Application.config.secret_token = '6d120cffccc3334805379b80267c5ab73b5a2b5f1d877a9330aa3fdf9b5bfff4caaa95a53527e81de8a1148e0fb92841eaaa655f96676b78a25211242ac1f9a8'
