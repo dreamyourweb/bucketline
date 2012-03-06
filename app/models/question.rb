b90123c269f2a051e38db365c661c1c4956834a5
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-03-06 22:40:09 +0100

    adding and removing items

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,8 @@ class Question
 	belongs_to :project
 	has_many :comments, :dependent => :destroy
 	has_many :items, :dependent => :destroy
+	
+	accepts_nested_attributes_for :items, :reject_if => lambda { |a| a[:description].blank? }, :allow_destroy => true
 
 	validates_presence_of :query
 	validates_format_of :type, :with => /^help\z|^tool\z|^material\z/
