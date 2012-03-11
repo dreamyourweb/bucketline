7c53a0b3fffca3237d17aa0c758ed375e5e41ce3
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-03-11 12:07:09 +0100

    arrow toggle

--- app/assets/javascripts/toggle_comments.js
+++ app/assets/javascripts/toggle_comments.js
@@ -3,6 +3,14 @@ $(function()
     $(".toggle").click(function() 
     {  
         // hides children divs if shown, shows if hidden 
-        $(this).prev().toggle(300); 
+        $(this).prev().toggle(300);
+				var arrow = $(this).find(".arrow");
+				if(arrow.hasClass("icon-chevron-down")) {
+					arrow.removeClass("icon-chevron-down");
+					arrow.addClass("icon-chevron-up");
+				} else {
+					arrow.removeClass("icon-chevron-up");
+					arrow.addClass("icon-chevron-down");
+				};
     }); 
 });
