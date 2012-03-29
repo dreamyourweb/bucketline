8416fda6dd628df7f48361bc048e4025502b1354
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-03-29 15:43:59 +0200

    registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :registerable
 
 	field :admin, :type => Boolean, :default => false  
 
