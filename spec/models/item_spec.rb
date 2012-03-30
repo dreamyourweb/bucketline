74cf98efac9150dfe84d5a2791e065ff7688004f
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-03-30 13:30:56 +0200

    empty item model specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Item do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe ".provided" do
+		before(:each) do
+			@project = Project.create(:query => "Mijn project", :start_at => Time.now, :end_at => Time.now + 1.day)
+			@item = @project.items.create(:name => "Mijn item", :type => "help", :notes => "", :location => "HvO", :amount => 1, :start_at => Time.now, :end_at => Time.now + 1.day)
+ 		end
+
+  	it "should return true if there are no items left to give" do
+		end
+
+		it "should return false if there are items left to give" do
+		end
+	end
 end
