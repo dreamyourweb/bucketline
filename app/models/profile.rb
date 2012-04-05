3f658642ec77ad1dd8aa5f3b9b15a6f3fe260bfa
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-04-05 13:55:25 +0200

    nested dates

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,9 @@ class Profile
 	has_event_calendar
 
 	embedded_in :user
-	has_many :available_dates	
+	has_many :available_dates, :autosave => true, :dependent => :delete	
+
+	accepts_nested_attributes_for :available_dates, :allow_destroy => true
 
   field :name, :type => String
   field :expertise, :type => String
