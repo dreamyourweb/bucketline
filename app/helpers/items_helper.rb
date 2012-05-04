1c978319ba7350b749d70538324a6edb06979cf3
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-05-04 22:06:40 +0200

    pretty usernames

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -6,14 +6,14 @@ module ItemsHelper
 			item.daypart.each do |daypart|
 				if item.daypart.length == 1
 					dayparts << daypart
-				elsif daypart == item.daypart
-						dayparts << daypart
+				elsif daypart == item.daypart.first
+					dayparts << daypart
 				elsif daypart == item.daypart.last(2).first
-						dayparts << daypart
+					dayparts << daypart
 				elsif daypart == item.daypart.last
-						dayparts << " en " + daypart
+					dayparts << " en " + daypart
 				else
-						dayparts << daypart + ", "
+					dayparts << daypart + ", "
 				end
 			end
 		end
@@ -23,11 +23,20 @@ module ItemsHelper
 	def get_contributors(item)
 		contributors = ""
 		if !item.profiles.empty?
-			contributors << " Bijdragers zijn"
+			contributors << " Bijdragers zijn "
 			item.profiles.each do |profile|
-				contributors << ", " + profile.name 
+				if item.profiles.length == 1
+					contributors << profile.name
+				elsif profile == item.profiles.first
+					contributors << profile.name
+				elsif profile == item.profiles.entries.last(2).first
+					contributors << profile.name
+				elsif profile == item.profiles.last
+					contributors << " en " + profile.name
+				else
+					contributors << profile.name + ", "
+				end
 			end
-			contributors << "."
 		end
 		contributors
 	end
