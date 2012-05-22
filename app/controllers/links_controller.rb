10ba4c4feeb9f92b6938e65cbed570f8a266abc6
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-05-22 17:24:36 +0200

    admin overzicht van alle bijdragen collapsable

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,7 +2,13 @@ class LinksController < ApplicationController
 	before_filter :authenticate_admin
 
 	def index
-		@links = Link.all
+		@links = Link.where(:item_start_at.gte => Date.today)
+		@projects = []
+		@links.each do |link|
+			@projects << link.project_query
+		end
+		@projects = @projects.uniq
+
 	end
 end
 
