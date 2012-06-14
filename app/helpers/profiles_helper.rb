7908c17ef391cd751c78e4967ff0ebcd80fa4880
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-06-14 14:15:39 +0200

    kopieerbare lijst met alle adressen

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -2,4 +2,16 @@ module ProfilesHelper
 	def get_amount(profile, item)
 		Link.where(:profile_id => profile.id, :item_id => item.id).first.amount
 	end
+
+	def build_emails_for_copy(users)
+		list = ""
+		users.each do |user|
+			if user == users.first
+				list << user.email
+			else
+				list << ", " + user.email
+			end
+		end
+		list
+	end
 end
