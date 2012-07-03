9c2310e9f958ce6146f0c7d515d4d818adb519b2
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-07-03 14:09:00 +0200

    amount for depricated links

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,6 +1,12 @@
 module ProfilesHelper
 	def get_amount(profile, item)
-		Link.where(:profile_id => profile.id, :item_id => item.id).first.amount
+		link = Link.where(:profile_id => profile.id, :item_id => item.id).first
+		if link.nil?
+			amount = "-"
+		else
+			amount = link.amount
+		end
+		amount
 	end
 
 	def build_emails_for_copy(users)
