35144afccbcfcfd9e234b26e926bdf1c47393ace
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-08-23 15:12:31 +0200

    specs refactored

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,51 +11,21 @@ describe Item do
 
 	describe ".provided" do
 		it "should mark an item as provided if there is nothing left to give" do
-			@item.link_to_profile(2, @profile)
-			@profile.link_to_item(2, @item)
+			Link.create(:item_id => @item.id, :profile_id => @profile.id, :amount => 2)
 			@item.provided.should == true			
 		end
 
 		it "should not mark an item as provided if there are items left to give" do
-			@item.link_to_profile(1, @profile)
-			@profile.link_to_item(1, @item)
+			Link.create(:item_id => @item.id, :profile_id => @profile.id, :amount => 1)
 			@item.provided.should == false			
-		end
-	end
-
-	describe ".link_to_profile" do
-		it "should link an item to a profile if the item is being provided" do
-			@item.link_to_profile(1, @profile)
-			@item.profiles.first.should == @profile
-		end
-	end
-
-	describe ".remove_profile" do
-		it "should remove an item from a profile if the item is being removed" do
-			@item.link_to_profile(1, @profile)
-			@item.profiles.first.should == @profile
-			@item.remove_profile(@profile)
-			@item.profiles.first.should == nil
 		end
 	end
 
 	describe ".items_left" do
 		it "should show the up-to-date amount of required items" do
 			@item.items_left.should == 2
-			@item.link_to_profile(1, @profile)
-			@profile.link_to_item(1, @item)
+			@item.links.create(:profile_id => @profile.id, :amount => 1)
 			@item.items_left.should == 1		
-		end
-	end
-
-	describe ".remove_links" do
-		it "should remove the links to profiles upon deletion of the item" do
-			Link.where(:item_id => @item.id, :profile_id => @profile.id).length.should == 0
-			@item.link_to_profile(1, @profile)
-			@profile.link_to_item(1, @item)
-			Link.where(:item_id => @item.id, :profile_id => @profile.id).length.should == 1
-			@item.destroy
-			Link.where(:item_id => @item.id, :profile_id => @profile.id).length.should == 0
 		end
 	end
 end
