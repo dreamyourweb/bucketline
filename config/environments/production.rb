db0128cf6229c58ce4e650aa75d81dd1f4216d6f
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-09-14 16:59:35 +0200

    domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,5 +62,5 @@ HvO::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'waardeverbinder.nl' }
+  config.action_mailer.default_url_options = { :host => 'bucketline.herokuapp.com' }
 end
