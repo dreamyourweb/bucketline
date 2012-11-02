ed0df249971d135b6dbabe7c298e733f9b2caefd
Andres Lamont <alamont@gmail.com>
2012-11-02 18:15:40 +0100

    Left menu in initiatives.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      initiative = current_user.user_roles.map{|ur| ur.initiative}.first
+      redirect_to root_url(:subdomain => initiative.name) 
+    end
   end
 end
