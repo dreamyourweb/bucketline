8bc009263cd4310fac8c7cf1f62a23706f6bab52
Thijs van de Laar <thijsvdlaar@hotmail.com>
2012-11-05 15:54:12 +0100

    redirect after login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       initiative = current_user.user_roles.map{|ur| ur.initiative}.first
-      redirect_to root_url(:subdomain => initiative.name) 
+      redirect_to root_url(:subdomain => initiative.slug) 
     end
   end
 end
