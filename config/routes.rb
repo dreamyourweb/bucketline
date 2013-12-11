9ec78c28037b4c2bba04132cec11af61060eee9b
Andres Lamont <alamont@gmail.com>
2013-12-11 15:55:07 +0100

    Some more fixes

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,14 @@ HvO::Application.routes.draw do
     get "contribution" => "profiles#contribution"
 	end
   
-  devise_for :users
+  # devise_for :users
+
+  devise_for :users, :skip => [:registrations]                                          
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+      patch 'users/:id' => 'devise/registrations#update', :as => 'user_registration'            
+    end
+
 	devise_scope :user do
 		get "login", :to => "devise/sessions#new"
 		get "logout", :to => "devise/sessions#destroy"
