90530b0054f16c48c385c0cbb405058740732be5
Andres Lamont <alamont@gmail.com>
2013-12-20 17:43:29 +0100

    Some redirects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,16 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       initiative = current_user.user_roles.map{|ur| ur.initiative}.first
-      if initiative
+      bucket_group = current_user.bucket_groups.first
+      if initiative.present?
       	redirect_to root_url(:subdomain => initiative.slug) 
+      elsif bucket_group.present?
+        redirect_to bucket_group_url(bucket_group, subdomain: false)
       else
-      	redirect_to "http://www.dreamyourapp.nl"
+        redirect_to profile_url(current_user.profile, :subdomain => false)
       end
+    else
+      redirect_to "http://www.dreamyourapp.nl"
     end
   end
 
